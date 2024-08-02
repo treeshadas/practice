a8a53cbf7b43bb8ebeb2f83e7b15ab2bd6d1ba84
treeshadas <113580966+treeshadas@users.noreply.github.com>
2024-08-02 18:08:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const app=express()
-const port=3000
+const port=4000
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 app.get('/k',(req, res)=>{
-    res.send("yes BABy")
+    res.send("yes kid")
     })
     app.get('/baby',(req,res)=>{
         res.send("Howdy")
